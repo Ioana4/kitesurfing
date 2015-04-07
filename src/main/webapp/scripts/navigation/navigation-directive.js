a4701dcaa21e47dbaf107c8e045685b663ec0b1c
Silviu <freemanfx@gmail.com>
2015-04-07 21:49:56 +0300

    make active

--- src/main/webapp/scripts/navigation/navigation-directive.js
+++ src/main/webapp/scripts/navigation/navigation-directive.js
@@ -4,8 +4,24 @@ angular.module('app')
            restrict: 'E',
            replace: false,
            templateUrl: 'views/navigation/navigation-directive.html',
-           controller: function () {
-        	   	
+           controller: function ($scope) {
+
+               $scope.menu = [
+                   {label: 'HOME', link: '#home'},
+                   {label: 'LESSONS', link: '#lessons'},
+                   {label: 'STORE', link: '#store'},
+                   {label: 'GALLERY', link: '#gallery'},
+                   {label: 'TRIPS', link: '#trips'},
+                   {label: 'CONTACT', link: '#contact'}
+               ];
+
+               $scope.setActive = function(element){
+                   $scope.activeElement = element;
+               };
+
+               $scope.getActive = function (element){
+                 return element == $scope.activeElement ? 'active': '';
+               }
            }
        }
     });
