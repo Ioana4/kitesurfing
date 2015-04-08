819af69a76277592feabf78256f0f7d46f909295
ioana marcu <iona@192-168-0-101.rdsnet.ro>
2015-04-09 01:38:15 +0300

    height 87% for carousel

---
+++ src/main/webapp/images/carousel/Din Kiteboarding_files/routes.js
@@ -0,0 +1,26 @@
+angular
+    .module('app')
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                  templateUrl: 'views/home/home.html'
+            })
+            .when('/about', {
+                  templateUrl: 'views/about/about.html'
+            })
+            .when('/lessons', {
+                  templateUrl: 'views/lessons/lessons.html'
+            })
+            .when('/store', {
+                  templateUrl: 'views/store/store.html'
+            })
+            .when('/gallery', {
+                  templateUrl: 'views/gallery/gallery.html'
+            })
+            .when('/travel', {
+                  templateUrl: 'views/travel/travel.html'
+            })
+            .when('/contact', {
+                  templateUrl: 'views/contact/contact.html'
+            });           
+    }]);
