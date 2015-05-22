eab78535838dbd82fd909464bf81c0aa3d9d6ec2
Ioana4 <ioana.marcu4@gmail.com>
2015-05-22 19:06:18 +0300

    Update navigation-directive.js

--- src/main/webapp/scripts/navigation/navigation-directive.js
+++ src/main/webapp/scripts/navigation/navigation-directive.js
@@ -12,10 +12,13 @@ angular.module('app')
                    {label: 'LESSONS', link: '#lessons'
                 	   , submenu: [
                 	       
-                	       {label: 'Free taste', link: '#freeTaste'}
-                	        {label: 'INTRODUCTION', link: '#lessons'}
-                	       {label: 'MODULE1', link: '#module1'}, {label: 'MODULE2', link: '#module2'},  {label: 'MODULE3', link: '#module3'},
-                	       {label: 'NEXT LEVEL', link: '#nextLevel'}, {label: 'FAQ', link: '#module4'}]},
+                	       {label: 'Free taste', link: '#freeTaste'} ,
+                	       {label: 'INTRODUCTION', link: '#lessons'},
+                	       {label: 'MODULE1', link: '#module1'}, 
+                	       {label: 'MODULE2', link: '#module2'}, 
+                	       {label: 'MODULE3', link: '#module3'},
+                	       {label: 'NEXT LEVEL', link: '#nextLevel'}, 
+                	       {label: 'FAQ', link: '#module4'}]},
                    {label: 'GALLERY', link: '#gallery'},
                    {label: 'VIDEO', link: '#video'},
                    {label: 'STORE', link: '#store'},
