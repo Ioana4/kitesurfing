fe8d0cbb0df16bca5275a5698f4933e12c6597d5
ioana marcu <iona@192-168-0-100.rdsnet.ro>
2016-02-02 18:15:59 +0200

    Brand fix

--- src/main/java/org/iona/kitesurf/service/NavigationService.java
+++ src/main/java/org/iona/kitesurf/service/NavigationService.java
@@ -5,27 +5,37 @@ import java.io.IOException;
 import org.iona.kitesurf.model.ImageFile;
 import org.iona.kitesurf.repository.ImageFileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class NavigationService {
-	
+
 	@Autowired
 	private ImageFileRepository imageFileRepository;
 
-	public ImageFile updateBrand(MultipartFile file) {
-//		try {
-//			
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-		return null;
+	@Value("${brandImageId:#{null}}")
+	private Long brandImageId;
+
+	public void updateBrand(MultipartFile file) {
+		try {
+			ImageFile brandImageFile = new ImageFile();
+			brandImageFile.setContent(file.getBytes());
+			//was previous uploaded.. so it should be updated
+			if (brandImageId != null) {
+				brandImageFile.setId(brandImageId);
+			}
+
+			ImageFile savedBrand = imageFileRepository.save(brandImageFile);
+			brandImageId = savedBrand.getId();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public ImageFile getBrand() {
-		return null;
+		return imageFileRepository.findOne(brandImageId);
 	}
-	
-	
+
 }
